6ac6fb7af3ad6dd72c7b666f8b25ad71dfc45407
Muhammad Rizky Saepul Putra <m.rizky.saepul.putra@gmail.com>
2025-12-24 12:21:16 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import json
 import numpy as np
 import os
-  
+      
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
